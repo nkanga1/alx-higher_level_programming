fe2deb4382691e917c773dab0792262e92d05661
nkanga1 <edunkanga@gmail.com>
2023-03-30 02:39:45 +0100

    tasks update 0x05. Python

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
-import sys
-
-
-def safe_print_integer_err(value):
-    """Prints an integer with "{:d}".format().
-
-    If a ValueError message is caught, a corresponding
-    message is printed to standard error.
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
 
     Args:
         value (int): The integer to print.
@@ -20,5 +14,4 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (False)
