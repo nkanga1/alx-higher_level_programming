0f317e7f8c7983c6d41fe8a708dada61c9ca88ee
nkanga1 <edunkanga@gmail.com>
2023-04-13 12:48:39 +0100

    made files executable

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,6 @@ import json
 
 
 def load_from_json_file(filename):
-    """Create a python object froma JSON file."""
+    """Create a Python object from a JSON file."""
     with open(filename) as f:
-        return json.loads(F)
+        return json.load(f)
